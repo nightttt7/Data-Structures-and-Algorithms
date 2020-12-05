2dbcf85cd9ca79329f2e111b22ffe80b6e370591
nightttt7 <nightttt7@gmail.com>
2020-12-05 12:29:09 +0100

    Q10 dynamic programming

--- leetcode/Q10.py
+++ leetcode/Q10.py
@@ -49,6 +49,30 @@ def isMatch(s, p):
 
 
 # 2. Dynamic Programming
+# cache intermediate results
+# improved from recursion
 def isMatch(s, p):
-    # TBC
-    pass
+    # a dict with 2-dim index and bool value
+    # index represent the index of s and p
+    cache = {}
+
+    def dp(i, j):
+        # if we find cache, use it!
+        # this is the core of dynamic programming
+        if (i, j) in cache:
+            return cache[i, j]
+        # or we solve it and add to cache
+        # code change from recursion
+        # be careful, now p and s never change, index i and j changes
+        elif j == len(p):
+            cache[i, j] = (i == len(s))
+        else:
+            first_match = i < len(s) and p[j] in {s[i], '.'}
+            if j+1 < len(p) and p[j+1] == '*':
+                cache[i, j] = (dp(i, j+2) or
+                               first_match and dp(i+1, j))
+            else:
+                cache[i, j] = first_match and dp(i+1, j+1)
+        return cache[i, j]
+
+    return dp(0, 0)

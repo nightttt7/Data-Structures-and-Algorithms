5dc143e37faebdcf560a48c815d0069db3a62fe3
nightttt7 <nightttt7@gmail.com>
2020-12-13 22:42:21 +0100

    tree (unfinished)

---
+++ leetcode/Tree.py
@@ -0,0 +1,25 @@
+# Tree
+
+# nodes: root, branches, leaves; childrem, parent, sibling
+# nodes: ancestor, descendant
+# degree of node: number of childern
+# degree of tree: maximum number of childern in tree
+# level: root in level 1, any childern level is parent level + 1
+# depth/height: maximum level in tree
+# edge: connects two nodes
+# path: ordered list of nodes that are connected by edges
+# binary tree: maximum two children
+
+# 1. List of Lists Representation
+myTree = ['a',  # root
+          ['b',  # left subtree
+           ['d', [], []],
+           ['e', [], []]],
+          ['c',  # right subtree
+           ['f', [], []],
+           []]
+          ]
+
+# 2. Representation using nodes and references
+
+# TBC

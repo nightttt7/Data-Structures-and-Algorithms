e6229f14a44e59ef0786aa6effad448045344292
nightttt7 <nightttt7@gmail.com>
2020-12-14 23:13:36 +0100

    tree (unfinished)

--- leetcode/Tree.py
+++ leetcode/Tree.py
@@ -20,6 +20,73 @@ myTree = ['a',  # root
            []]
           ]
 
-# 2. Representation using nodes and references
 
-# TBC
+# 2. Representation using nodes and references
+class BinaryTree:
+    def __init__(self, val=None):
+        self.val = val
+        self.left_child = None
+        self.right_child = None
+
+    def insert_left(self, val=None):
+        if self.left_child:
+            t = BinaryTree(val)
+            t.left_child = self.left_child
+            self.left_child = t
+        else:
+            self.left_child = BinaryTree(val)
+
+    def insert_right(self, val=None):
+        if self.right_child:
+            t = BinaryTree(val)
+            t.right_child = self.right_child
+            self.right_child = t
+        else:
+            self.right_child = BinaryTree(val)
+
+
+# Parse Tree
+def parse_tree(expression):
+    elist = expression.split()
+    # node_list: to save ancestors
+    node_list = []
+    etree = BinaryTree()
+    curr = etree
+    # think about it
+    node_list.append(curr)
+
+    for x in elist:
+        if x == '(':
+            node_list.append(curr)
+            curr.insert_left()
+            curr = curr.left_child
+        elif x in ('+', '-', '*', '/'):
+            node_list.append(curr)
+            curr.val = x
+            curr.insert_right()
+            curr = curr.right_child
+        elif x == ')':
+            curr = node_list.pop()
+        else:
+            curr.val = float(x)
+            curr = node_list.pop()
+
+    return etree
+
+
+# solve parsed tree, recursion
+def solve(tree):
+    import operator
+    symbols = {'+': operator.add, '-': operator.sub,
+               '*': operator.mul, '/': operator.truediv}
+    left, right = tree.left_child, tree.right_child
+    if left and right:
+        return symbols[tree.val](solve(left), solve(right))
+    else:
+        return tree.val
+
+
+# expression = "( ( 10 + 5 ) * 3 )"
+# expression = "( 3 + ( 4 * 5 ) )"
+# t = parse_tree(expression)
+# a = solve(t)

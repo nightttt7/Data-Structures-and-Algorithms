0fdbd36c7cd947a106ed41392e84b8118be0dfe8
nightttt7 <nightttt7@gmail.com>
2021-03-09 18:43:36 +0800

    Graph BFS

--- leetcode/Graph.py
+++ leetcode/Graph.py
@@ -5,6 +5,10 @@
 
 
 class Vertex(object):
+    dist = None
+    prev = None
+    color = 'white'
+
     def __init__(self, key):
         self.id = key
         self.connected_to = {}
@@ -48,3 +52,51 @@ class Graph(object):
 # for v in g:
 #     for w in v.connected_to.keys():
 #         print("( %s , %s )" % (v.id, w.id))
+
+
+# Word Ladder Problem
+def build_graph(word_list):
+    d = {}
+    g = Graph()
+    # create buckets of words that differ by one letter
+    for word in word_list:
+        for i in range(len(word)):
+            bucket = word[:i] + '_' + word[i+1:]
+            if bucket in d:
+                d[bucket].append(word)
+            else:
+                d[bucket] = [word]
+    # add vertices and edges for words in the same bucket
+    for bucket in d.keys():
+        for word1 in d[bucket]:
+            for word2 in d[bucket]:
+                if word1 != word2:
+                    g.add_edge(word1, word2)
+    return g
+
+
+word_list = ['foil', 'fail', 'fall', 'pall', 'poll', 'pool',
+             'cool', 'fool', 'foul', 'pole', 'pope', 'pale',
+             'sale', 'page', 'sage']
+g = build_graph(word_list)
+
+
+# Breadth First Search (BFS)
+# start is a Vertex
+def bfs(start):
+    start.dist = 0
+    vert_queue = []
+    vert_queue.append(start)
+    while (len(vert_queue) > 0):
+        vert_curr = vert_queue.pop()
+        for nbr in vert_curr.connected_to:
+            if nbr.color == 'white':
+                nbr.color = 'gray'
+                nbr.dist = vert_curr.dist + 1
+                nbr.pred = vert_curr
+                vert_queue.append(nbr)
+        vert_curr.color = 'black'
+
+
+bfs(g.vertexes['fool'])
+g.vertexes['sage'].dist

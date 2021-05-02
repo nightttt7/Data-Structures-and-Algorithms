7124632d7cce286bc31c72eafb234b231aedc819
nightttt7 <nightttt7@gmail.com>
2021-05-02 23:19:47 +0800

    DFS general

--- Data-Structures-and-Algorithms/Graph.py
+++ Data-Structures-and-Algorithms/Graph.py
@@ -192,4 +192,31 @@ def orderByAvail(n):
     return [y[1] for y in resList]
 
 
-# TBC
+# DFS: General Depth First Search
+class DFSGraph(Graph):
+    def __init__(self):
+        super().__init__()
+        self.time = 0
+
+    def dfs(self):
+        for aVertex in self:
+            aVertex.setColor('white')
+            aVertex.setPred(-1)
+        for aVertex in self:
+            if aVertex.getColor() == 'white':
+                self.dfsvisit(aVertex)
+
+    def dfsvisit(self, startVertex):
+        startVertex.setColor('gray')
+        self.time += 1
+        startVertex.setDiscovery(self.time)
+        for nextVertex in startVertex.getConnections():
+            if nextVertex.getColor() == 'white':
+                nextVertex.setPred(startVertex)
+                self.dfsvisit(nextVertex)
+        startVertex.setColor('black')
+        self.time += 1
+        startVertex.setFinish(self.time)
+
+
+# Topological Sorting

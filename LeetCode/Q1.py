b551e8f3c5ab4aefa8c3483c821c66097b9d9caa
nightttt7 <nightttt7@gmail.com>
2021-08-15 00:12:52 +0800

    add method 3

--- LeetCode/Q1.py
+++ LeetCode/Q1.py
@@ -23,6 +23,24 @@ def twoSum(nums, target):
 # enumerate: return both index and item
 
 
+# 3
+# similiar with Q167, two sum (input array is sorted)
+# for better understanding: https://www.cxyxiaowu.com/9769.html
+def twoSum(nums, target):
+    nums_index = [(v, index) for index, v in enumerate(nums)]
+    nums_index.sort()
+    begin = 0
+    end = len(nums) - 1
+    while True:
+        curr = nums_index[begin][0] + nums_index[end][0]
+        if curr == target:
+            return [nums_index[begin][1], nums_index[end][1]]
+        elif curr < target:
+            begin += 1
+        else:
+            end -= 1
+
+
 # test
 twoSum([2, 7, 11, 15], 9)
 # [0, 1]
